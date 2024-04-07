1a893570a91d007406831c0d644149fd0d3f1f13
raaasin <nisarvskp@gmail.com>
2024-04-07 15:15:10 +0530

    trial

--- new.py
+++ new.py
@@ -34,43 +34,6 @@ chain = (
     | StrOutputParser()
 )
 
-print(chain.invoke("""
-        For the following query, if it requires drawing a table, reply as follows:
-                  The Data column can only have numerical value
-        {"table": {"columns": ["column1", "column2", ...], "data": [[value1, value2, ...], [value1, value2, ...], ...], "facts": ["Based on the data you provided, write an interesting insight or fact here"]}} 
+print(chain.invoke("What bank failed in North Carolina?"))
 
-        If the query requires creating a bar chart, reply as follows:
-        {"bar": {"columns": ["A", "B", "C", ...], "data": [25, 24, 10, ...], "facts": ["Based on the data you provided, write an interesting insight or fact here"]}}
-        
-        If the query requires creating a line chart, reply as follows:
-        {"line": {"columns": ["A", "B", "C", ...], "data": [25, 24, 10, ...], "facts": ["Based on the data you provided, write an interesting insight or fact here"]}}
-
-        If the query requires creating a scatter plot, reply as follows:
-        {"scatter": {"columns": ["X", "Y"], "data": [[x1, y1], [x2, y2], ...], "facts": ["Based on the data you provided, write an interesting insight or fact here"]}}
-
-        There can only be three types of charts: "bar", "line", and "scatter".
-
-        If the query is asking a question that requires neither chart nor calculation, reply as follows:
-        {"answer": "answer"}
-        Example:
-        {"answer": "The title with the highest rating is 'Gilead'"}
-
-        If the query is asking for some calculation or reasoning, provide the answer along with the proof and description.
-        Example:
-        {"answer": "The sum of the numbers is 10. Here's how it was calculated: 1 + 2 + 3 + 4 = 10"}
-
-        If you do not know the answer, reply as follows:
-        {"answer": "I do not know."}
-
-        Return all output as a string.
-
-        All strings in "columns" list and data list should be in double quotes,
-
-        For example: {"columns": ["title", "ratings_count"], "data": [["Gilead", 361], ["Spider's Web", 5164]]}
-
-        Let's think step by step.
-
-        Below is the query.
-        Query: 
-        """
-        + "make some sort of bar chart from bank data"))
+"""The bank that failed in North Carolina is Blue Ridge Savings Bank, Inc."""
